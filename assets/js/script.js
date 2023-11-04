8d34de59e066a721d8cb7c9a530ee43a4fe13e53
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-11-04 18:08:23 +0100

    Added arithmetic operation script

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("display");
+    const buttons = document.querySelectorAll("input[type='button']");
+
+    for (const button of buttons) {
+        const value = button.value;
+
+        button.addEventListener("click", function () {
+            const value = this.value;
+
+            if (value === "=") {
+                try {
+                    display.value = eval(display.value);
+                } catch (err) {
+                    display.value = "Error";
+                }
+            } else if (value === "AC") {
+                display.value = "";
+            } else {
+                display.value += value;
+            }
+        });
+    }
+});
