4cfe4f24775b7fb30933df5f41ff1df06970e3cc
Benjamin Ohene Konadu <benohenekonadu@outlook.com>
2023-11-04 18:11:07 +0100

    added comments to script functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
+// Dom document object model
 document.addEventListener("DOMContentLoaded", function () {
     const display = document.getElementById("display");
     const buttons = document.querySelectorAll("input[type='button']");
 
+    // script for calculator buttons
     for (const button of buttons) {
         const value = button.value;
 
+        // add event listener to each button
         button.addEventListener("click", function () {
             const value = this.value;
 
